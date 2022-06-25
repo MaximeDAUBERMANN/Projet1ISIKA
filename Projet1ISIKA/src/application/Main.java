105af99957e91d1b2f675e3b6f9685c88b2ea9da
GhTasnim <tasnim.gharbii@gmail.com>
2022-06-25 10:55:31 +0200

    AffichageAnnuaire

--- Projet1ISIKA/src/application/Main.java
+++ Projet1ISIKA/src/application/Main.java
@@ -1,18 +1,15 @@
 package application;
-	
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-
-
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,605,499);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -22,8 +19,6 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-		System.out.println("Test");
-		System.out.println("Test1");
 		launch(args);
 	}
 }
